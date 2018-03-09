5a8e4bc1a6919487f8ad6c650c3d0bf3aa7a8561
Drew McMillan <drew.mcmillan@bbc.co.uk>
2018-03-09 14:27:36 +0000

    Update package.json

---
+++ dist/options.js
@@ -0,0 +1,23 @@
+'use strict';
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports.loadOptions = loadOptions;
+var loaderUtils = require('loader-utils');
+
+function loadOptions(loader) {
+  var options = loaderUtils.getOptions(loader);
+
+  var buildPath = options.buildPath ? options.buildPath : undefined;
+  if (buildPath === undefined) {
+    throw new Error('You have to specify build path, where the WASM files will be stored');
+  }
+
+  var emccPath = options.emccPath ? options.emccPath : process.platform === 'win32' ? 'em++.bat' : 'em++';
+  var emccFlags = options.emccFlags ? options.emccFlags : ['-O3'];
+
+  return {
+    buildPath, emccPath, emccFlags
+  };
+}
